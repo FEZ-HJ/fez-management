906880db63fd05496de1f0d51254a93df50ad8ec
FEZ <fez_hj@163.com>
2019-01-11 17:15:40 +0800

    Initial commit

---
+++ src/main/java/com/fez/FezManagementApplication.java
@@ -0,0 +1,14 @@
+package com.fez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FezManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FezManagementApplication.class, args);
+    }
+
+}
+
